c3e6f29cac610e99a8abb24183dc4bdfeb759094
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-08-10 03:54:13 +0800

    Fibonaccimal Base
    fib建表 board[i] = board [i-1]+board[i-2]
    從Board 最大往前找
    flag = 1 就push back 0
    記得呼叫函數

---
+++ Fibonaccimal Base.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> b(40);
+void fb(){
+    b[0] = 0;
+    b[1] = 1;
+    b[2] = 1;
+    for (int i = 2; i <= 39;i++){
+        b[i] = b[i - 1] + b[i - 2];
+    }
+}
+
+int main()  {
+    int t;
+    cin >> t;
+    fb();
+  
+    while(t--){
+        vector<int> ans;
+        int flag=0;
+        int n;
+        cin >> n;
+        int temp = n;
+
+        for (int i = 39; i > 1;i--){
+            if(n>=b[i]){
+                n = n - b[i];
+                ans.push_back(1);
+                flag = 1;
+                
+            }
+            else if (flag==1){
+                ans.push_back(0);
+            }
+        }
+        cout << temp << " = ";
+        for(auto i:ans){
+            cout << i;
+        }
+        cout << " (fib)" << endl;
+    }
+
+    return 0;
+}

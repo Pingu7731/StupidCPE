045395839fc8465516af735a2a7e95583c35600a
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-09-12 01:56:41 +0800

    Decode the mad man
    REMEMBER USE TOLOWER TO FIND
    
    k[k.find(tolower(s[i]))]

---
+++ decode the mad man.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+    string t;
+    string k = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+
+    while (getline(cin, t))
+    {
+        for (int i = 0; i < t.size(); i++)
+        {
+            if (t[i] == ' ')
+            {
+                cout << t[i];
+            }else {
+              cout << k[k.find(tolower(t[i])) - 2];
+            }
+        }
+        cout << endl;
+        }
+        return 0;
+}

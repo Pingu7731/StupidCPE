30d332d55f12f559537410e0834587a3822f39d6
Ping <altonchai@gmail.com>
2024-09-19 13:05:22 +0800

    Was WA in virtual judge
    check number   if (v[i] <= 0)
                {
                    flag = false;
                }
                if (v[i] != 0 && v[i - 1] >= v[i])
                {
                    flag = false;
                }

--- B2-Sequence.cpp
+++ B2-Sequence.cpp
@@ -2,35 +2,55 @@
 
 using namespace std;
 
-int main (){
+int main()
+{
     int t;
-     int cases = 0;
-    while (cin >> t){
+    int cases = 0;
+    while (cin >> t)
+    {
         cases++;
         set<int> check;
         bool flag = true;
         vector<int> v(t);
-        for (int i = 0; i < t;i++){
+        for (int i = 0; i < t; i++)
+        {
             cin >> v[i];
+
+            if (v[i] <= 0)
+            {
+                flag = false;
+            }
+            if (v[i] != 0 && v[i - 1] >= v[i])
+            {
+                flag = false;
+            }
         }
-        for (int i = 0; i < t-1;i++){
-            for (int j = i; j < t;j++){
+
+        for (int i = 0; i < t - 1; i++)
+        {
+            for (int j = i + 1; j < t; j++)
+            {
                 int temp = v[i] + v[j];
-                if (check.count (temp)){
+                if (check.count(temp))
+                {
                     flag = false;
                     break;
                 }
                 check.insert(temp);
             }
-            if(!flag){
+            if (!flag)
+            {
                 break;
             }
         }
         cout << "Case #" << cases;
-        if(flag){
+        if (flag)
+        {
             cout << ": It is a B2-Sequence." << endl
                  << endl;
-        }else {
+        }
+        else
+        {
             cout << ": It is not a B2-Sequence." << endl
                  << endl;
         }

6776b2ac70d32be24118ba11f5925b6d0a5ae2b8
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-09-24 06:04:34 +0800

    Random reveal number
    lmao i gave up and i use chat gpt to help me

---
+++ sohai.cpp
@@ -0,0 +1,63 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main (){
+//     string ans;
+//     string character = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+{}|QWERTYUIOPASDFGHJKLZXCVBNM";
+//     while (getline(cin,ans))
+//     {
+        
+//         for (auto &i:ans){
+
+//             if(i ==' '){
+//                 cout << ' ';
+//             }
+//             for(auto c:character){
+//                 cout << c<<endl;
+//                 // if(c==i){
+
+//                 // }
+//             }
+            
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+#include <bits/stdc++.h>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main() {
+    string ans;
+   
+    string character = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+{}|QWERTYUIOPASDFGHJKLZXCVBNM";
+
+    
+
+    while (getline(cin, ans)) {
+        string revealed = "";  
+        for (auto i : ans) {
+            if (i == ' ') {
+                revealed += ' ';
+                cout << revealed << endl;
+            } else {
+                char actualChar = i;  
+                
+                for (int k = 0; k < 10; k++) {  
+                    char randomChar = character[rand() % character.size()];
+                    cout << revealed + randomChar << "\r"; 
+                    fflush(stdout);  
+                    // this_thread::sleep_for(chrono::milliseconds(50));  
+                }
+                revealed += actualChar;  
+                cout << revealed << endl; 
+            }
+        }
+        cout << endl; 
+    }
+    return 0;
+}

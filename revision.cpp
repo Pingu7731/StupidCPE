bb465e1474e1cbbdd61a0d6cb37ea60d0de81447
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-10-04 19:24:27 +0800

    a
    a

--- revision.cpp
+++ revision.cpp
@@ -2,26 +2,34 @@
 
 using namespace std;
 
+bool c(pair<int,int>a,pair<int,int>b){
+    if(a.second!=b.second){
+        return a.second < b.second;
+    }else{
+        return a.first > b.first;
+    }
+}
 int main (){
     string s;
     bool flag = false;
     while(getline(cin,s)){
-        for (int i = 0; i < s.size();i++){
-            if(s[i]==' '){
-                cout << ' ';
-            }
-            if(s[i]=='"'&&flag ==false){
-                s[i] = '`';
-                s.insert( s.begin() + i,'`');
-                flag = true;
-            }
-            if(s[i]=='"'&&flag==true){
-                s[i] = '\'';
-                s.insert(s.begin() + i, '\'');
-                flag = false;
-            }
+        map<int, int> m;
+        vector<pair<int, int>> v;
+        if(flag==true){
+            cout << endl;
         }
-        cout << s<<endl;
+        flag = true;
+        for (int i = 0; i < s.size(); i++){
+            m[s[i]]++;
+        }
+        for(auto i:m){
+            v.push_back(i);
+        }
+        sort(v.begin(), v.end(), c);
+
+        for(auto i:v){
+            cout << i.first << " " << i.second << endl;
+        }
 
     }
     return 0;
